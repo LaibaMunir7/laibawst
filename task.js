639f8747743b7b4612e5f213be68eaf78640229e
LaibaMunir7 <laibamunir499@gmail.com>
2025-10-07 15:46:05 -0700

    yes

---
+++ task.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Hello = () => {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement('h1', null, 'Hello World')
+  );
+};
+
+export default Hello;
